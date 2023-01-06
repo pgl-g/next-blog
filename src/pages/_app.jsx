c8327cbb08cdcf15557781c0b57aafc5302b1300
pgl <1801688332@qq.com>
2023-01-06 14:02:19 +0800

    feat: nprogress 进度条优化

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,13 @@
+import Router from 'next/router'
+import nProgress from 'nprogress'
+
 import '@/styles/globals.css'
+import '@/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-
+import Layout from "@/components/layout/Layout";
+import Seo from "@/components/Seo";
 
 
 export default function SingleBlogPage() {
   return (
-    <div>
+    <Layout>
+      <Seo 
+        templateTitle='Blog'
+        description="Showcase of my projects on front-end development that I'm proud of."
+      />
       详情页
-    </div>
+    </Layout>
   )
 }
