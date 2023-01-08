8add476477e717aec6eb5df2ffd0baa726242792
pgl <1801688332@qq.com>
2023-01-08 23:24:50 +0800

    feat: 背景动态粒子

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,21 +1,22 @@
 import * as React from "react";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-import Plum from '@/components/layout/Plum'
+import Plum from "@/components/layout/Plum";
+// import ReactCanvasNest from 'react-canvas-nest';
 
 export default function Layout({ children }) {
-
-  
   return (
     <>
       <Header />
-      <div className="layout">
+      <div className="z-10 layout">
         <div className="mx-8">
           {children}
           <Footer />
-          </div>
+        </div>
       </div>
       <Plum />
+
+      {/* <ReactCanvasNest /> */}
     </>
   );
 }
