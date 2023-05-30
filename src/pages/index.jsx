22ca3da4e366e2ec6ddfc73d2c5fcfa139f6172e
guxiaobiao <guxiaobiao@xinxuan.net>
2023-05-30 19:38:00 +0800

    fix: change

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <Layout>
       <Seo />
-      <div className="flex justify-center items-center  mx-8 mt-12 mb:mx-0 ">
+      <div className="flex justify-center items-center text-white mx-8 mt-12 mb:mx-0 ">
         <TypeIt options={{ breakLines: true, html: true }}>
-          <strong className="py-4 leading-tight text-white font-light font-mono">
+          <strong className="py-4 leading-tight  font-light font-mono">
             🤔 Hello, 我是PGL。很高兴与你相遇，一同学习知识。
             希望你能玩得开心！！
           </strong>
